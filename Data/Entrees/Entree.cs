a8a40391cdb15ac18290cd320e9cf5cfd3b9bb66
jake73-lab <jake73@ksu.edu>
2020-09-29 13:49:58 -0500

    added entree parent class

---
+++ Data/Entrees/Entree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BleakwindBuffet.Data.Entrees
+{
+    public class Entree : IOrderItem
+    {
+        public double Price { get; }
+        public uint Calories { get; }
+        public List<string> SpecialInstructions { get; }
+
+        public Entree()
+        {
+        }
+    }
+}
